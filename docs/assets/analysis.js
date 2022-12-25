b117084febc912d798699316bcb769b32fd5417e
你们的小f <me@fzf404.art>
2022-12-25 17:48:22 +0800

    :pencil: 更新缩进

--- docs/assets/analysis.js
+++ docs/assets/analysis.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
-(function() {
+(function () {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?eec9312dcd08fb96194e92589598e85a";
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
